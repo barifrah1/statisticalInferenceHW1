33e1bab13b172856e480cea938275cc38ebd0fea
barifrah1 <63462394+barifrah1@users.noreply.github.com>
2021-11-11 22:19:47 +0200

    q4 fixes

--- q4.py
+++ q4.py
@@ -4,12 +4,15 @@ from numpy import random
 import random as rand
 import sklearn
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import pylab
 REAL_BETAS = np.array([0.05,0.3,0.2])
 X_MEAN = 0
 X_STD = 1
 NUM_TO_DRAW_A = 20
+MAX_TRAIN_SIZE = 1000
+TRAIN_SIZE_STEP = 10
 
 def calculateLamda(x1,x2,b=REAL_BETAS):
     result = np.exp(np.dot(b,np.array([1,x1,x2])))
@@ -41,13 +44,13 @@ if __name__ == '__main__':
     #xArray is 20 couples of numbers randomly distributed from normal(0,1) distribution
     yArray,xArray,lamdasArray = drawY(NUM_TO_DRAW_A,X_MEAN,X_STD)
     #fitting a poisson regressor with log link function
-    clf = linear_model.PoissonRegressor()
+    clf = linear_model.PoissonRegressor(alpha=0)
     clf.fit(xArray,yArray)
     #printing betas
     betas = [clf.intercept_,clf.coef_[0],clf.coef_[1]]
     print(f"beta0:{clf.intercept_}, beta1:{clf.coef_[0]}, beta2:{clf.coef_[1]}")
     #drawing 10000 examples from using betas
-    yArrayTest,xArrayTest,lamdasArrayTest = drawY(10000,X_MEAN,X_STD,b=betas)
+    yArrayTest,xArrayTest,lamdasArrayTest = drawY(10000,X_MEAN,X_STD)
     #poisson loss as found in the internet  - sum(y_i-realY_i*log(y_i))/N
     yTestPred = clf.predict(xArrayTest)
     loss = calculatePoissonLoss(yTestPred,yArrayTest)
@@ -55,14 +58,13 @@ if __name__ == '__main__':
     # poisson loss for differnt n: 10,20, … ,1000
     ranges = []
     losses = []
-    for n in range(10,1000,10):
-        yArray,xArray,lamdasArray = drawY(n,X_MEAN,X_STD,b=REAL_BETAS)
-        clf = linear_model.PoissonRegressor()
+    for n in range(10,MAX_TRAIN_SIZE,TRAIN_SIZE_STEP):
+        yArray,xArray,lamdasArray = drawY(n,X_MEAN,X_STD)
+        clf = linear_model.PoissonRegressor(alpha=0)
         clf.fit(xArray,yArray)
         betas = [clf.intercept_,clf.coef_[0],clf.coef_[1]]
-        yArrayTest,xArrayTest,lamdasArrayTest = drawY(10000,X_MEAN,X_STD,b=betas)
+        yArrayTest,xArrayTest,lamdasArrayTest = drawY(MAX_TRAIN_SIZE,X_MEAN,X_STD)
         yTestPred = clf.predict(xArrayTest)
-        #loss = clf.score(xArrayTest,yArrayTest)
         loss = calculatePoissonLoss(yTestPred,yArrayTest)
         ranges.append(n)
         losses.append(loss)
@@ -72,7 +74,7 @@ if __name__ == '__main__':
     # naming the y axis
     plt.ylabel('poisson loss')
     # giving a title to my graph
-    plt.title('My first graph!')
+    plt.title('poisson loss by train set size')
     # calc the trendline
     z = np.polyfit(ranges, losses, 1)
     p = np.poly1d(z)
@@ -81,4 +83,42 @@ if __name__ == '__main__':
     plt.show()
     print(f"test losses are: {losses}")
     
-        
+    #As we can see, the loss decreases when the train set size increases.
+    #This make sense as long as the train size increases the approximated betas converge to the real betas
+    #The test set betas are draw from the real betas distribution so when the train size gets bigger the gap between the distribution gets smaller and thats why the loss decreases two. 
+    ranges = []
+    poissonLosses = []
+    lrLosses = []
+    for n in range(10,MAX_TRAIN_SIZE,TRAIN_SIZE_STEP):
+        yArray,xArray,lamdasArray = drawY(n,X_MEAN,X_STD)
+        #poisson regression
+        clf = linear_model.PoissonRegressor(alpha=0)
+        clf.fit(xArray,yArray)
+        betas_poisson = [clf.intercept_,clf.coef_[0],clf.coef_[1]]
+        #linear regression
+        lr = linear_model.LinearRegression()
+        lr.fit(xArray,yArray)
+        betas_lr = [lr.intercept_,lr.coef_[0],lr.coef_[1]]
+        yArrayTest,xArrayTest,lamdasArrayTest = drawY(MAX_TRAIN_SIZE,X_MEAN,X_STD)
+        yTestPredPoisson = clf.predict(xArrayTest)
+        yTestPredLr = lr.predict(xArrayTest)
+        poissonLoss = mean_squared_error(yArrayTest,yTestPredPoisson)
+        lrLoss = mean_squared_error(yArrayTest,yTestPredLr)
+        poissonLosses.append(poissonLoss)
+        lrLosses.append(lrLoss)
+        ranges.append(n)
+    plt.plot(ranges,poissonLosses,label="poisson regression")
+    plt.plot(ranges,lrLosses, label="lr")
+    # naming the x axis
+    plt.xlabel('numer of examples in the train set')
+    # naming the y axis
+    plt.ylabel('MSE')
+    # giving a title to my graph
+    plt.title('MSE by train set size')
+    plt.legend()
+    # calc the trendline
+    # z = np.polyfit(ranges, losses, 1)
+    # p = np.poly1d(z)
+    # pylab.plot(ranges,p(ranges),"r--")    
+    # function to show the plot
+    plt.show()    
